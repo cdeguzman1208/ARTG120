17ee4bfd57741cc56d61e2ab3c25ae776f8be939
Beatrice Yu <besyu@ucsc.edu>
2023-05-15 18:58:15 -0700

    player constructor

--- FinalProject/player.js
+++ FinalProject/player.js
@@ -1,10 +1,5 @@
 class Player extends Phaser.Physics.Arcade.Sprite {
-<<<<<<< HEAD
     constructor(scene, velocity) {
         super(scene, x, y, 'player');
-=======
-    constructor() {
-        
->>>>>>> 291ce099bcb229a04300d4da7a3282912fee327b
     }
 }
