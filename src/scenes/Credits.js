8eab7d0f9908eb3d2f88e09e72c45bcf8d574438
Cromwell <cdeguzman1208@gmail.com>
2023-05-18 16:36:08 -0700

    set up scene switcher

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -4,6 +4,25 @@ class Credits extends Phaser.Scene {
     }
 
     create() {
+        // set up scene switcher
+        this.input.keyboard.on('keydown', (event) => {
+            switch(event.key) {
+                case 'Escape':
+                    // console.log('menu')
+                    this.scene.start('menuScene')
+                    break
+                case ' ':
+                    // console.log('play')
+                    this.scene.start('playScene')
+                    break
+                case 'Backspace':
+                    // console.log('credits')
+                    this.scene.restart
+                    break
+                default:
+                    break
+            }
+        })
     }
 
     update() {
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,6 +4,25 @@ class Menu extends Phaser.Scene {
     }
 
     create() {
+        // set up scene switcher
+        this.input.keyboard.on('keydown', (event) => {
+            switch(event.key) {
+                case 'Escape':
+                    // console.log('menu')
+                    this.scene.restart
+                    break
+                case ' ':
+                    // console.log('play')
+                    this.scene.start('playScene')
+                    break
+                case 'Backspace':
+                    // console.log('credits')
+                    this.scene.start('creditsScene')
+                    break
+                default:
+                    break
+            }
+        })
     }
 
     update() {
