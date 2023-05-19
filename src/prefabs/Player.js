3b60d3fa9402f2a55a2034d27ff64bb2ffec76c2
David <david.amaya20@yahoo.com>
2023-05-18 17:03:59 -0700

    test commit (david)

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -1,16 +1,37 @@
 class Player extends Phaser.Physics.Arcade.Sprite {
-    constructor(scene, velocity) {
-        super(scene, x, y, 'player')
+    constructor(scene, x, y) {
+        super(scene, x, y, 'player', 0);
 
         // maintain scene context 
         this.parentScene = scene
 
         // set up physics sprite 
-        this.parentScene.add.existing(this)
-        this.parentScene.physics.add.existing(this)
+        this.parentScene.add.existing(this); 
+        this.parentScene.physics.add.existing(this);
+        this.body.set
+
+        // Properties
+        this.stage = 1;
+        this.maxScrap = 10;
+        this.nScrap = 0;
+        this.maxVelocity = 700;
+        this.velocity = this.maxVelocity;
     }
 
     update() {
 
+    }
+
+    collect() {
+        if(this.nScrap >= this.maxScrap){
+            this.velocity -= 10;
+        }
+        this.nScrap++;
+    }
+
+    evolve() {
+        this.stage++;
+        this.maxVelocity += 200;
+        this.velocity = this.maxVelocity;
     }
 }
