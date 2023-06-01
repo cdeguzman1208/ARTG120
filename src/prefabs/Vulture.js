a054a76fe285f24c9cae59211d701a73f235126b
David <david.amaya20@yahoo.com>
2023-06-01 16:09:11 -0700

    implemented vulture

--- src/prefabs/Vulture.js
+++ src/prefabs/Vulture.js
@@ -1,15 +1,26 @@
 class Vulture extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y, speed, patrolDist) {
-        super(scene, x, y, '');
+        super(scene, x, y, 'vulture');
+
+        // maintain scene context 
+        this.parentScene = scene
+
+        // setup physics sprite
+        this.parentScene.add.existing(this)
+        this.parentScene.physics.add.existing(this)
 
         // properties
         this.travelling = false;
         this.patrolDist = patrolDist;
         this.speed = speed;
+        this.patrolling = true;
 
         // target coordinates
-        this.targetX = this.x;
-        this.targetY = this.y;
+        this.targetX = x;
+        this.targetY = y;
+
+        // start flying animation
+        this.play('fly');
     }
 
     update() {
@@ -18,12 +29,20 @@ class Vulture extends Phaser.Physics.Arcade.Sprite {
 
     chooseDestination() {
         // make patrol destination the edge of a square for now
-        this.targetX = Phaser.Math.Between(-1*this.patrolDist, this.patrolDist);
-        this.targetY = Phaser.Math.Between(-1*this.patrolDist, this.patrolDist);
-        let vX = (targetX - this.x) / this.speed;
-        let vY = (targetY - this.y) / this.speed;
+        this.targetX = Phaser.Math.Between(this.x - this.patrolDist, this.x + this.patrolDist);
+        this.targetY = Phaser.Math.Between(this.y - this.patrolDist, this.y + this.patrolDist);
+        let vX = (this.targetX - this.x); 
+        let vY = (this.targetY - this.y);
         this.setVelocity(vX, vY);
-        
+        // console.log(`vX = ${this.body.velocity.x}`)
+        // console.log(`vY = ${this.body.velocity.y}`)
+
+        this.patrolTime = this.parentScene.time.delayedCall(1000, () => {
+            this.setVelocity(0,0);
+            this.waitTime = this.parentScene.time.delayedCall(2000, () => {
+                this.chooseDestination();
+            })
+        })
     }
 
 }
