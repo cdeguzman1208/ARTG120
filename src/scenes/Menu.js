4534dc602cef95e200745147034fbb16baed5787
Cromwell <cdeguzman1208@gmail.com>
2023-05-31 18:18:13 -0700

    add sfx

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,23 +4,29 @@ class Menu extends Phaser.Scene {
     }
 
     create() {
+        this.selectSFX = this.sound.add('select')
+        
         // set up scene switcher
         this.input.keyboard.on('keydown', (event) => {
             // console.log(event.key);
             switch(event.key) {
                 case 'Escape':
                     // console.log('menu')
+                    this.selectSFX.play()
                     this.scene.restart
                     break
                 case ' ':
                     // console.log('play')
+                    this.selectSFX.play()
                     this.scene.start('playScene')
                     break
                 case 'Backspace':
                     // console.log('credits')
+                    this.selectSFX.play()
                     this.scene.start('creditsScene')
                     break
                 case 't' :
+                    this.selectSFX.play()
                     this.scene.start('tutorialScene')
                     break
                 default:
