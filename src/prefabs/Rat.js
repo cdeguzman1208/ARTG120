bb356c3423fd74d1c739794f222601831554b554
Beatrice Yu <besyu@ucsc.edu>
2023-06-04 17:34:49 -0700

    working rats

--- src/prefabs/Rat.js
+++ src/prefabs/Rat.js
@@ -3,18 +3,25 @@ class Rat extends Phaser.Physics.Arcade.Sprite {
         let side = Math.floor(Math.random() * 4); 
         // top -> right -> bottom -> left 
         if (side == 0) {
+            // console.log("top");
             super(scene, Math.random() * (map.widthInPixels - 64) + 32, -32, 'rat'); 
+            this.side = 0; 
         }
         else if (side == 1) {
+            // console.log("right");
             super(scene, map.widthInPixels + 32, Math.random() * (map.heightInPixels - 64) + 32, 'rat'); 
+            this.side = 1; 
         }
         else if (side == 2) {
+            // console.log("bottom");
             super(scene, Math.random() * (map.widthInPixels - 64) + 32, map.heightInPixels + 32, 'rat'); 
+            this.side = 2; 
         }
         else {
+            // console.log("left");
             super(scene, -32, Math.random() * (map.heightInPixels - 64) + 32, 'rat'); 
+            this.side = 3; 
         }
-        // super(scene, x, y, 'enemy')
 
         // maintain scene context 
         this.parentScene = scene
@@ -23,13 +30,13 @@ class Rat extends Phaser.Physics.Arcade.Sprite {
         this.parentScene.add.existing(this)
         this.parentScene.physics.add.existing(this)
 
-        if (side == 0) {
+        if (this.side == 0) {
             this.setVelocity(0, 100); 
         }
-        else if (side == 1) {
+        else if (this.side == 1) {
             this.setVelocity(-100, 0); 
         }
-        else if (side == 2) {
+        else if (this.side == 2) {
             this.setVelocity(0, -100); 
         }
         else {
@@ -42,14 +49,80 @@ class Rat extends Phaser.Physics.Arcade.Sprite {
         this.anims.play('ratWalk');
     }
 
+    steal() {
+        this.scrap = true; 
+
+        if (this.side == 0) {
+            this.setVelocity(0, -100); 
+            this.side = 2; 
+        }
+        else if (this.side == 1) {
+            this.setVelocity(100, 0); 
+            this.side = 3; 
+        }
+        else if (this.side == 2) {
+            this.setVelocity(0, 100); 
+            this.side = 0; 
+        }
+        else {
+            this.setVelocity(-100, 0); 
+            this.side = 1; 
+        }
+    }
+
+    scurry() {
+        this.scrap = true; 
+
+        if (this.side == 0) {
+            this.setVelocity(0, -200); 
+            this.side = 2; 
+        }
+        else if (this.side == 1) {
+            this.setVelocity(200, 0); 
+            this.side = 3; 
+        }
+        else if (this.side == 2) {
+            this.setVelocity(0, 200); 
+            this.side = 0; 
+        }
+        else {
+            this.setVelocity(-200, 0); 
+            this.side = 1; 
+        }
+    }
+
     update() {
         if (ratCount < 5) {
             this.parentScene.addRat(this.parent); 
             ratCount++; 
+            // console.log(ratCount)
         }
 
         this.rotation = this.body.angle; 
 
-        
+        if ((this.side == 0) && (this.y > (this.height + map.heightInPixels))) {
+            // console.log("-top");
+            this.destroy();
+            ratCount--;
+            // console.log(ratCount)
+        }
+        else if ((this.side == 1) && (this.x < -this.width)) {
+            // console.log("-right");
+            this.destroy();
+            ratCount--;
+            // console.log(ratCount)
+        }
+        else if ((this.side == 2) && (this.y < -this.height)) {
+            // console.log("-bottom");
+            this.destroy();
+            ratCount--;
+            // console.log(ratCount)
+        }
+        else if ((this.side == 3) && (this.x > (this.width + map.widthInPixels))) {
+            // console.log("-left");
+            this.destroy();
+            ratCount--;
+            // console.log(ratCount)
+        }
     }
 }
